f65109448cac9b5dac25f346555f058f60ab3be3
s1n7ax <srineshnisala@gmail.com>
2025-08-03 10:54:44 +0530

    chore: code clean up

--- lua/plugins/auto-session/init.lua
+++ lua/plugins/auto-session/init.lua
@@ -1,8 +1,7 @@
 vim.o.sessionoptions =
 	'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
 
+---@type AutoSession.Config
 require('auto-session').setup({
 	suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-	-- log_level = 'debug',
 })
-
