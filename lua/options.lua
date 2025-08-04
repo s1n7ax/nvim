d673960eca63bdbe2410a09fb97b331e0e7c16a2
s1n7ax <srineshnisala@gmail.com>
2025-08-04 23:58:53 +0530

    chore: update options to lazyvim defaults

--- lua/options.lua
+++ lua/options.lua
@@ -1,29 +1,48 @@
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.scrolloff = 5
-vim.o.signcolumn = 'yes:2'
+vim.o.autowriteall = true
+vim.o.clipboard = vim.env.SSH_TTY and '' or 'unnamedplus'
 vim.o.cmdheight = 0
-vim.o.more = false
+vim.o.confirm = true
+vim.o.cursorline = true
+vim.o.expandtab = true
 vim.o.hlsearch = false
 vim.o.ignorecase = true
-vim.o.autowriteall = true
-vim.o.swapfile = false
-
--- Window split behavior
-vim.o.splitright = true
-vim.o.splitbelow = true
-
--- Disable status line
 vim.o.laststatus = 0
-
--- Disable text wrapping
-vim.o.wrap = false
-
--- Enable clipboard support
-vim.o.clipboard = 'unnamedplus'
-vim.o.splitkeep = 'cursor'
-
+vim.o.linebreak = true
 vim.o.messagesopt = 'hit-enter,history:5000'
+vim.o.more = false
+vim.o.number = true
+vim.o.pumblend = 10
+vim.o.relativenumber = true
+vim.o.scrolloff = 6
+vim.o.shiftround = true
+vim.o.shiftwidth = 2
+vim.o.showmode = false
+vim.o.sidescrolloff = 8
+vim.o.signcolumn = 'yes:2'
+vim.o.smartcase = true
+vim.o.smartindent = true
+vim.o.smoothscroll = true
+vim.o.splitbelow = true
+vim.o.splitkeep = 'cursor'
+vim.o.splitright = true
+vim.o.swapfile = false
+vim.o.tabstop = 2
+vim.o.termguicolors = true
+vim.o.undofile = true
+vim.o.undolevels = 10000
+vim.o.wildmode = 'longest:full,full'
+vim.o.winminwidth = 5
+vim.o.wrap = false
+vim.opt.shortmess:append({ W = true, I = true, c = true, C = true })
+vim.opt.spelllang = { 'en' }
+vim.opt.fillchars = {
+	foldopen = '',
+	foldclose = '',
+	fold = ' ',
+	foldsep = ' ',
+	diff = '╱',
+	eob = ' ',
+}
 
 vim.g.s1n7ax_window_horizontal_percentage = 0.8 -- Focused window width percentage
 vim.g.s1n7ax_window_vertical_percentage = 0.7 -- Focused window height percentage
