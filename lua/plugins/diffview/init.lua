06de58c8030aaeba6e5e102b2ecc0ee7b7a4de0c
s1n7ax <srineshnisala@gmail.com>
2025-08-05 15:57:27 +0530

    feat: diff view keymap to diff current branch against main branch

--- lua/plugins/diffview/init.lua
+++ lua/plugins/diffview/init.lua
@@ -3,10 +3,30 @@ local mapper = utils.mapper
 local nmap = mapper('n')
 local vmap = mapper('x')
 
+local function diffview_default_branch()
+	local handle = io.popen(
+		'git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null | sed "s@^refs/remotes/origin/@@"'
+	)
+
+	if not handle then
+		return vim.cmd.DiffviewFileHistory()
+	end
+
+	local default_branch = handle:read('*a'):gsub('%s+', '')
+	handle:close()
+
+	if default_branch == '' then
+		default_branch = 'main'
+	end
+
+	vim.cmd.DiffviewOpen(default_branch)
+end
+
 -- stylua: ignore
 nmap({
 	{ '<leader>ii', '<cmd>DiffviewFileHistory %<cr>', 'Git diff file' },
-	{ '<leader>in', '<cmd>DiffviewFileHistory<cr>', 'Git diff branch' },
+	{ '<leader>in', diffview_default_branch, 'Git diff main branch' },
+	{ '<leader>it', '<cmd>DiffviewFileHistory<cr>', 'Git diff branch' },
 })
 
 vmap({
