5c494659cdeba6346b5275a582b01bcf6313431e
s1n7ax <srineshnisala@gmail.com>
2025-08-29 13:11:48 +0530

    feat: enable prettierd

--- lua/plugins/conform/init.lua
+++ lua/plugins/conform/init.lua
@@ -2,6 +2,8 @@ require('conform').setup({
 	formatters_by_ft = {
 		lua = { 'stylua' },
 		nix = { 'nixfmt' },
+		javascript = { 'prettierd' },
+		typescript = { 'prettierd' },
 	},
 	format_on_save = {
 		-- These options will be passed to conform.format()
