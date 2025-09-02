d86504db98f371301cca4eddf6e75a925ec594f8
s1n7ax <srineshnisala@gmail.com>
2025-09-02 17:06:24 +0530

    chore: enable lsp snippets

--- lua/plugins/blink/init.lua
+++ lua/plugins/blink/init.lua
@@ -5,8 +5,8 @@ blink.setup({
 		implementation = 'prefer_rust',
 		prebuilt_binaries = { force_version = 'v1.6.0' },
 	},
-	signature = { enabled = true, window = { border = 'single' } },
-
+	signature = { enabled = true, window = { border = 'rounded' } },
+	snippets = { preset = 'luasnip' },
 	sources = {
 		-- add lazydev to your completion providers
 		default = { 'lazydev', 'lsp', 'path', 'buffer' },
@@ -18,13 +18,15 @@ blink.setup({
 				score_offset = 100,
 			},
 		},
-		transform_items = function(_, items)
-			-- removes snippes from completion items
-			return vim.tbl_filter(function(item)
-				local kind = require('blink.cmp.types').CompletionItemKind
-				return (item.kind ~= kind.Snippet and item.kind ~= kind.Copilot)
-			end, items)
-		end,
+
+		-- disable all snippets coming from LSP
+		-- transform_items = function(_, items)
+		-- 	-- removes snippes from completion items
+		-- 	return vim.tbl_filter(function(item)
+		-- 		local kind = require('blink.cmp.types').CompletionItemKind
+		-- 		return (item.kind ~= kind.Snippet and item.kind ~= kind.Copilot)
+		-- 	end, items)
+		-- end,
 	},
 	keymap = {
 		preset = 'none',
@@ -41,41 +43,3 @@ blink.setup({
 		},
 	},
 })
-
-return {
-	'saghen/blink.cmp',
-	optional = true,
-	enabled = true,
-	dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
-	---@type blink.cmp.Config
-	opts = {
-		snippets = { preset = 'luasnip' },
-		signature = { enabled = true, window = { border = 'single' } },
-		sources = {
-			default = { 'lsp', 'path', 'buffer' },
-
-			transform_items = function(_, items)
-				-- removes snippes from completion items
-				return vim.tbl_filter(function(item)
-					local kind = require('blink.cmp.types').CompletionItemKind
-					return (item.kind ~= kind.Snippet and item.kind ~= kind.Copilot)
-				end, items)
-			end,
-		},
-
-		keymap = {
-			preset = 'none',
-			['<c-space>'] = { 'show' },
-			['<c-n>'] = { 'select_next', 'snippet_forward', 'fallback' },
-			['<c-e>'] = { 'select_prev', 'snippet_backward', 'fallback' },
-			['<cr>'] = { 'select_and_accept', 'fallback' },
-		},
-		completion = {
-			menu = { border = 'single' },
-			documentation = { window = { border = 'single' } },
-			ghost_text = {
-				enabled = false,
-			},
-		},
-	},
-}
