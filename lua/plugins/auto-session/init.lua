0c0eaad21659c60422ea66f768a13cba73efdef4
s1n7ax <srineshnisala@gmail.com>
2026-02-17 12:27:32 +0530

    chore: update auto-session config and plugin lock file

--- lua/plugins/auto-session/init.lua
+++ lua/plugins/auto-session/init.lua
@@ -1,6 +1,10 @@
-vim.opt.sessionoptions:remove({ 'help', 'tabpages', 'terminal' })
+vim.opt.sessionoptions:remove({ 'help', 'tabpages', 'terminal', 'winsize' })
+
+-- vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 ---@type AutoSession.Config
 require('auto-session').setup({
 	suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+	git_use_branch_name = true,
+	git_auto_restore_on_branch_change = true,
 })
