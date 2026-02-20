18bc158c1eddf25dd5f0e7301993da0d098373cd
s1n7ax <srineshnisala@gmail.com>
2026-02-21 00:15:42 +0530

    chore: enable some luasnip keymaps

--- lua/plugins/luasnip/init.lua
+++ lua/plugins/luasnip/init.lua
@@ -1,28 +1,57 @@
+local utils = require('utils')
+local nmap = utils.keymaps.mapper({ 'n' })
+
 local helper = require('plugins.luasnip.helper')
 
 helper.register_snippets()
 -- stylua: ignore
+vim.keymap.set({ 'i', 's' }, '<c-i>', helper.expand_or_jump(), { desc = '(Snippet) Expand or jump' })
 vim.keymap.set(
-	{ 'i', 's' }, '<c-i>', helper.expand_or_jump(), { desc = '(Snippet) Expand or jump' })
+	'n',
+	'<c-i>',
+	helper.expand_or_jump(),
+	{ desc = '(Snippet) Expand or jump' }
+)
 
--- return {
--- 	{ '<Tab>', '<Tab>', desc = 'Tab Space', mode = { 'i', 's' } },
--- 	{
--- 		'<c-i>',
--- 		helper.expand_or_jump(),
--- 		desc = '(Snippet) Expand or jump',
--- 		mode = { 'i', 's' },
--- 	},
--- 	{
--- 		'<c-m>',
--- 		helper.jump_prev(),
--- 		desc = '(Snippet) Jump prev placeholder',
--- 		mode = { 's' },
--- 	},
--- 	{
--- 		'<c-l>',
--- 		helper.change_choice(),
--- 		desc = '(Snippet) Change choice',
--- 		mode = { 'i', 's' },
--- 	},
--- }
+-- stylua: ignore
+-- nmap({
+-- 	{ { 'i', 's' }, '<c-i>', helper.expand_or_jump(), { desc = '(Snippet) Expand or jump' } },
+-- 	{ { 'i', 's' }, '<c-i>', helper.expand_or_jump(), { desc = '(Snippet) Expand or jump' } },
+-- })
+
+vim.keymap.set({ 'i', 's' }, '<Tab>', '<Tab>', {desc = 'Tab Space'})
+-- vim.keymap.set(
+-- 	{ 'i', 's' },
+-- 	'<c-m>',
+-- 	helper.jump_prev(),
+-- 	{ desc = '(Snippet) Jump prev placeholder' }
+-- )
+
+vim.keymap.set(
+	{ 'i', 's' },
+	'<c-l>',
+	helper.change_choice(),
+	{ desc = '(Snippet) Change choice' }
+)
+
+return {
+	-- { '<Tab>', '<Tab>', desc = 'Tab Space', mode = { 'i', 's' } },
+	-- {
+	-- 	'<c-i>',
+	-- 	helper.expand_or_jump(),
+	-- 	desc = '(Snippet) Expand or jump',
+	-- 	mode = { 'i', 's' },
+	-- },
+	-- {
+	-- 	'<c-m>',
+	-- 	helper.jump_prev(),
+	-- 	desc = '(Snippet) Jump prev placeholder',
+	-- 	mode = { 's' },
+	-- },
+	-- {
+	-- 	'<c-l>',
+	-- 	helper.change_choice(),
+	-- 	desc = '(Snippet) Change choice',
+	-- 	mode = { 'i', 's' },
+	-- },
+}
