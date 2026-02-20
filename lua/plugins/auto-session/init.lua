21e09f452f0f8dacf263015be83c6c88a2569296
s1n7ax <srineshnisala@gmail.com>
2026-02-20 10:08:22 +0530

    refactor(auto-session): format sessionoptions configuration

--- lua/plugins/auto-session/init.lua
+++ lua/plugins/auto-session/init.lua
@@ -1,6 +1,13 @@
-vim.opt.sessionoptions:remove({ 'help', 'tabpages', 'terminal', 'winsize' })
-
--- vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.opt.sessionoptions:remove({
+	'blank',
+	'buffers',
+	'curdir',
+	'folds',
+	'help',
+	'tabpages',
+	'winsize',
+	'terminal',
+})
 
 ---@type AutoSession.Config
 require('auto-session').setup({
