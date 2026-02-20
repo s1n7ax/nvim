4bb0cc817df4cf5aa7e87d673c16aa69b386c040
s1n7ax <srineshnisala@gmail.com>
2026-02-21 00:19:40 +0530

    fix: remove opencode util module reference

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -3,7 +3,6 @@ local M = {}
 M.keymaps = require('utils.keymaps')
 M.editing = require('utils.editing')
 M.lsp = require('utils.lsp')
-M.opencode = require('utils.opencode')
 M.git = require('utils.git')
 M.tui = require('utils.tui')
 M.context = require('utils.context')
